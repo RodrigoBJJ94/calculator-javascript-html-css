7eb7fd5b39ffb4a0326c9e9f57da51557c5a4328
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-07-24 22:26:38 -0300

    Version 1.6

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,14 @@ function Calculator() {
         this.catchClicks();
         this.catchEnter();
     };
+
     this.catchEnter = () => {
         document.addEventListener('keypress', event => {
             if (event === 13);
             this.equalNumberDisplay();
         });
     };
+
     this.catchClicks = () => {
         document.addEventListener('click', event => {
             const element = event.target;
@@ -19,6 +21,7 @@ function Calculator() {
             if (element.classList.contains('button-equal')) this.equalNumberDisplay();
         });
     };
+
     this.equalNumberDisplay = () => {
         try {
             const count = eval(this.display.value);
@@ -32,12 +35,16 @@ function Calculator() {
             return;
         }
     };
+
     this.addNumberDisplay = element => {
         this.display.value += element.innerText;
         this.display.focus();
     };
+
     this.clearNumberDisplay = () => this.display.value = '';
+
     this.deleteNumberDisplay = () => this.display.value = this.display.value.slice(0, -1);
 }
+
 const calculator = new Calculator();
 calculator.start();
